e941aae3a0b92f206aa9f1f32ae3772d4502acb4
Charc1213 <35974327+Charc1213@users.noreply.github.com>
2023-01-17 12:44:47 -0800

    resume

--- resume.js
+++ resume.js
@@ -1,10 +1,16 @@
 function switchIframe() {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 600) {
-        document.getElementById("video-iframe").src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX8_PHtMM&#x2F;view?embed";
-    } else {
-        document.getElementById("video-iframe").src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX0Cd6SCg&#x2F;view?embed";
-    }
+   // Get the iframe element
+var iframe = document.getElementById("resume");
+
+// Check if the device is a mobile or desktop
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   // If mobile, change the src of the iframe
+   iframe.src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX8_PHtMM&#x2F;view?embed";
+} else {
+   // If desktop, change the src of the iframe
+   iframe.src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX0Cd6SCg&#x2F;view?embed";
+}
+
 }
 
 // Call the function when the page is loaded
