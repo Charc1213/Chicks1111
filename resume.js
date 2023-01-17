c10e66c2d3332b3f2c41e866c0f54c9621cfca10
Charc1213 <35974327+Charc1213@users.noreply.github.com>
2023-01-17 13:25:54 -0800

    fixing line

--- resume.js
+++ resume.js
@@ -1,18 +1,10 @@
 function switchImage() {
-    // Get the current screen size
-var screenWidth = window.innerWidth;
-var screenHeight = window.innerHeight;
-
-// Select the SVG image element
-var img = document.getElementById("resumeImage");
-
-// Check the screen size and swap the image source accordingly
-if (screenWidth < 480 || screenHeight < 480) {
-  img.src = "Images/Mobile Resume of Chantelle Hicks.svg";
-} else {
-  img.src = "Images/Resume of Chantelle Hicks.svg";
-}
-
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        var img = document.getElementById("resumeImage");
+        img.src = "Images/Mobile Resume of Chantelle Hicks.svg";
+     }
+     
+    
 }
 
 // Call the function when the page is loaded
