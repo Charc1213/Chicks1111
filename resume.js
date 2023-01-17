05a5765184ec0108d96e4050d0250e190ebdbb6b
Charc1213 <35974327+Charc1213@users.noreply.github.com>
2023-01-17 13:15:43 -0800

    Changing to image

--- resume.js
+++ resume.js
@@ -1,20 +1,14 @@
-function switchIframe() {
-   // Get the iframe element
-var iframe = document.getElementById("resume");
-
-// Check if the device is a mobile or desktop
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   // If mobile, change the src of the iframe
-   iframe.src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX8_PHtMM&#x2F;view?embed";
-} else {
-   // If desktop, change the src of the iframe
-   iframe.src = "https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFX0Cd6SCg&#x2F;view?embed";
-}
-
+function switchImage() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        var img = document.getElementById("resumeImage");
+        img.src = "Images\Mobile Resume of Chantelle Hicks.svg";
+     }
+     
+    
 }
 
 // Call the function when the page is loaded
-window.onload = switchIframe;
+window.onload = switchImage;
 
 // Call the function when the window is resized
-window.addEventListener("resize", switchIframe);
+window.addEventListener("resize", switchImage);
